605e51d00dc58ac1069c42eaa68683bf1f9fa32f
Bible <ptang8@bus.illinois.edu>
2019-06-02 20:53:54 +0900

    feat: add footer component

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,35 +1,26 @@
-import React from 'react';
-
-const hrStyle = {
-  marginTop: 75,
-};
+import { backgroundColor } from './constants';
 
 const Footer = () => (
-  <div>
-    <hr style={hrStyle} />
-    <p>
-      <span role="img" aria-label="microscope">
-        🔬 A <a href="https://postlight.com">Postlight Labs</a> project.
-      </span>
-      <span role="img" aria-label="fork">
-        🍴
-      </span>{' '}
-      <a href="https://github.com/postlight/headless-wp-starter">
-        Fork on GitHub
-      </a>
-      .
-    </p>
-    <p>
-      <span role="img" aria-label="wave">
-        👋
-      </span>{' '}
-      Need help with your publishing platform?{' '}
-      <a href="mailto:hello@postlight.com?subject=Partner+with+Postlight+on+a+headless+CMS+project">
-        Contact us
-      </a>
-      .
-    </p>
-  </div>
+  <footer>
+    <figure>
+      <img className="media" src="/static/images/logo.svg" alt="logo" />
+    </figure>
+    <style jsx>
+      {`
+        footer {
+          display: flex;
+          justify-content: center;
+          background-color: ${backgroundColor};
+          padding-top: 20px;
+          padding-bottom: 20px;
+        }
+        .media {
+          height: 50px;
+          width: 100%;
+        }
+      `}
+    </style>
+  </footer>
 );
 
 export default Footer;
